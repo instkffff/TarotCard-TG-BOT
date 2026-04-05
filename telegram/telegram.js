eed2ae8256ca470f524c6a6dfdeda8b797820efd
NightCandle <instkffff@gmail.com>
2026-04-06 02:21:39 +0800

    .

--- telegram/telegram.js
+++ telegram/telegram.js
@@ -35,7 +35,7 @@ function FinalMessage(user_id) {
     return {
         type: 'photo',
         media: url + rotation + '/' + cardID + '.jpg',
-        caption: "== " + cardName + " == " + cardMean + '</br>' + pick_pool_string, // 这里的文字会显示在图片下方
+        caption: "== " + cardName + " == " + cardMean + '\n' + pick_pool_string, // 这里的文字会显示在图片下方
         parse_mode: 'HTML'     // 如果文字里有加粗等标签，请保留
     };
 
